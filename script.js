b388ec80dc3e92068859a73856a25f9c080e4b98
Skeptic <skepticsameer@gmail.com>
2025-08-03 13:31:11 -0500

    Style updates to definitions

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const definitions = {
-  en: "Apostophobia (n.)<br>The irrational fear, hatred or punishment of people who leave their religion.",
-  ar: "أفوفوبيا (اسم)<br>الخوف أو الكراهية أو العقاب غير العقلاني للأشخاص الذين يتركون دينهم.",
-  fr: "Apostophobie (n.f.)<br>La peur, la haine ou la punition irrationnelle des personnes qui quittent leur religion.",
-  fa: "اپوستوفوبیا<br>ترس، نفرت یا مجازات غیرمنطقی افرادی که دین خود را ترک می‌کنند.",
-  de: "Apostophobie (f)<br>Die irrationale Angst, der Hass oder die Bestrafung von Menschen, die ihre Religion verlassen.",
-  ur: "اپوسٹو فوبیا<br>وہ غیر منطقی خوف، نفرت یا سزا جو مذہب چھوڑنے والوں کے خلاف ہو۔",
+  en: "<strong>Apostophobia</strong> (n.)<br><br>The irrational fear, hatred or punishment of people who leave their religion.",
+  ar: "<strong>أفوفوبيا</strong> (اسم)<br><br>الخوف أو الكراهية أو العقاب غير العقلاني للأشخاص الذين يتركون دينهم.",
+  fr: "<strong>Apostophobie</strong>  (n.f.)<br><br>La peur, la haine ou la punition irrationnelle des personnes qui quittent leur religion.",
+  fa: "<strong>اپوستوفوبیا</strong><br><br>ترس، نفرت یا مجازات غیرمنطقی افرادی که دین خود را ترک می‌کنند.",
+  de: "<strong>Apostophobie</strong>  (f)<br><br>Die irrationale Angst, der Hass oder die Bestrafung von Menschen, die ihre Religion verlassen.",
+  ur: "<strong>اپوسٹو فوبیا</strong><br><br>وہ غیر منطقی خوف، نفرت یا سزا جو مذہب چھوڑنے والوں کے خلاف ہو۔",
   hi: "अपोस्टोफोबिया (संज्ञा)<br>जो लोग अपना धर्म छोड़ते हैं उनके प्रति अव्यवहारिक डर, नफरत या सज़ा।"
 };
 
